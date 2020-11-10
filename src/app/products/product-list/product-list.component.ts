6fe433c20448380c62d7b939ee2ea33749ad2a92
ldex <laurent.duveau+git@gmail.com>
2020-11-10 10:26:47 -0500

    Pause matin jour 2

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from '../product.interface';
 
@@ -11,8 +13,30 @@ import { Product } from '../product.interface';
 export class ProductListComponent implements OnInit {
 
   title = "Products";
-  products: Product[];
+  //products: Product[];
+  products$: Observable<Product[]>;
   selectedProduct: Product;
+  errorMessage;
+  productNb = 0;
+
+  // Pagination
+  pageSize = 5;
+  start = 0;
+  end = this.pageSize;
+  currentPage = 1;
+
+  previousPage() {
+    this.start -= this.pageSize;
+    this.end -= this.pageSize;
+    this.currentPage--;
+    this.selectedProduct = null;
+  }
+  nextPage() {
+    this.start += this.pageSize;
+    this.end += this.pageSize;
+    this.currentPage++;
+    this.selectedProduct = null;
+  }
 
   onSelect(product: Product): void {
     this.selectedProduct = product;
@@ -21,7 +45,25 @@ export class ProductListComponent implements OnInit {
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
+    this.products$ = this
+                      .productService
+                      .products$
+                      .pipe(
+                        tap(products => this.productNb = products.length),
+                        catchError(
+                          error => {
+                            this.errorMessage = "Error while loading products";
+                            return EMPTY;
+                          }
+                        )
+                      );
+
+    // this
+    //   .productService
+    //   .products$
+    //   .subscribe(
+    //     results => this.products = results
+    //   )
   }
 
 }
